bf6744a7e6eceefdc8f41552113505136a1fb6de
TALEX1995 <tanase.alex@libero.it>
2023-08-03 18:01:08 +0200

    Added card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,9 +4,23 @@
 
 @section('main-content')
 
-  <main>
+  <main class="main-comics">
     <section class="jumbotron">
       <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="Jumbo">
+    </section>
+
+    <section id="current-series">
+      <div class="container card-container">
+        <div class="card">
+          <div>
+            <img class="img-card"
+              src="https://e0.pxfuel.com/wallpapers/416/298/desktop-wallpaper-superman-cartoon-superman-comic-book-thumbnail.jpg"
+              alt="">
+          </div>
+          <h3>Action Comics</h3>
+        </div>
+      </div>
+
     </section>
   </main>
 
